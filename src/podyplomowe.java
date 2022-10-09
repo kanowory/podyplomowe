26462ae85c67214c9225186be18aee3c6d6ed0bd
kanowory <kanowory@student.wszib.edu.pl>
2022-10-09 14:26:52 +0200

    dodano informację o nazwie uczelni

--- src/podyplomowe.java
+++ src/podyplomowe.java
@@ -2,5 +2,7 @@ public class podyplomowe {
     public static void main(String[] args) {
         String poziom = "podyplomowe";
         System.out.println("Poziom studiów: " + poziom);
+        String uczelnia = "WSZiB";
+        System.out.println("Nazwa uczelni: " + uczelnia);
     }
 }
